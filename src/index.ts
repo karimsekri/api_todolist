33f15f6c6e550c38dca77a16ca4a0441c78c0e0c
karim <karim.sekri@gmail.com>
2023-09-08 20:37:09 +0200

    Ajout des requetes find et create

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import sequelize from "sequelize";
-import tedious from "tedious";
+import sequelize, { DataTypes , Sequelize} from "sequelize";
+//import tedious from "tedious";
+
 
 
 const app = express();
@@ -11,16 +12,22 @@ const server = process.env.SERVER as string;
 const dataBaseName = process.env.DATABASE as string;
 const userName = process.env.USER as string;
 const pwd = process.env.PASSWORD as string;
+const dbPort =parseInt(process.env.DBPORT as string);
 
 
 //Initiation de la connexion
-const mySequelize =  new sequelize.Sequelize(dataBaseName,userName,pwd,{
-    dialect: 'mssql',    
-    host: server, 
-    dialectOptions: {
-        encrypt: true
-    }  
-});
+// const mySequelize =  new sequelize.Sequelize(dataBaseName,userName,pwd,{
+//     dialect: 'postgres',    
+//     host: server, 
+//     port: dbPort,
+//     dialectOptions: {
+//         encrypt: true
+//     }  
+// });
+const mySequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./db.sqlite",
+  });
 
 
 //Pour tester la connection 
@@ -33,10 +40,36 @@ async function connexionTest() {
       }
     }
 
-   // connexionTest();
+    //connexionTest();
 
-app.get("/hellos", (_, res) => {    
-    res.send();   
+//Creation du model
+const maTache = mySequelize.define('Tache', {
+   
+    tacheName:{
+        type : DataTypes.STRING,
+        allowNull:false
+   },
+    tacheIsChecked:{
+        type : DataTypes.BOOLEAN,
+        allowNull : false
+    }
+});
+//Verifier la correspondance du model est de la table 
+console.log(maTache  === mySequelize.models.Tache);
+
+//This creates the table, dropping it first if it already existed
+maTache.sync();
+
+
+
+app.get("/maTacheCreate", async (_, res) => {   
+    const creerMaTache = await maTache.create({tacheName : "jouer", tacheIsChecked : true});
+    res.send(creerMaTache);   
+});
+
+app.get("/maTacheFindAll", async (_, res) => {   
+    const maListeDeTache =  await maTache.findAll();     
+    res.send(maListeDeTache);   
 });
 
 app.listen( myport, () =>
