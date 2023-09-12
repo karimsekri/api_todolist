effd51dc225fa99189c7ac349c1514aaa1ac7a14
karim <karim.sekri@gmail.com>
2023-09-12 10:44:16 +0200

    Modification des API

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,12 @@ import sequelize, { DataTypes , Sequelize} from "sequelize";
 
 const app = express();
 dotenv.config();
-const myport = parseInt(process.env.PORT as string);
-const server = process.env.SERVER as string;
-const dataBaseName = process.env.DATABASE as string;
-const userName = process.env.USER as string;
-const pwd = process.env.PASSWORD as string;
-const dbPort =parseInt(process.env.DBPORT as string);
+ const myport = parseInt(process.env.PORT as string);
+// const server = process.env.SERVER as string;
+// const dataBaseName = process.env.DATABASE as string;
+// const userName = process.env.USER as string;
+// const pwd = process.env.PASSWORD as string;
+// const dbPort =parseInt(process.env.DBPORT as string);
 
 
 //Initiation de la connexion
@@ -57,19 +57,50 @@ const maTache = mySequelize.define('Tache', {
 //Verifier la correspondance du model est de la table 
 console.log(maTache  === mySequelize.models.Tache);
 
-//This creates the table, dropping it first if it already existed
+//This creates the table
 maTache.sync();
 
 
-
-app.get("/maTacheCreate", async (_, res) => {   
-    const creerMaTache = await maTache.create({tacheName : "jouer", tacheIsChecked : true});
+app.get("/maTacheCreate/:tacheName", async (req, res) => { 
+    const tacheName = req.params.tacheName;  
+    const creerMaTache = await maTache.create({tacheName : tacheName, tacheIsChecked : false});
     res.send(creerMaTache);   
 });
 
 app.get("/maTacheFindAll", async (_, res) => {   
     const maListeDeTache =  await maTache.findAll();     
     res.send(maListeDeTache);   
+});
+
+app.delete("/maTacheDelete/:tacheNameId", async (req, res) => { 
+    const tacheNameID = req.params.tacheNameId;  
+    const deleteMaTache =  await maTache.destroy({
+        where:{
+            id : tacheNameID
+        }
+});  
+   
+    res.send("deleted");   
+});
+
+app.get("/upDateRowTache/true/:id/", async (req, res) => { 
+    const tacheID = req.params.id;  
+    const updateMaTache = await maTache.update({tacheIsChecked : true},{
+        where :{
+            id : tacheID
+        }
+    });
+    res.send(updateMaTache);   
+});
+
+app.get("/upDateRowTache/false/:id/", async (req, res) => { 
+    const tacheID = req.params.id;  
+    const updateMaTache = await maTache.update({tacheIsChecked : false},{
+        where :{
+            id : tacheID
+        }
+    });
+    res.send(updateMaTache);   
 });
 
 app.listen( myport, () =>
